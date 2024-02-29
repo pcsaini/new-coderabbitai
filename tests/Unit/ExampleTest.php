f7fbdb7c678a0107b76a9709fe6b0285c5940ff7
Prem Chand Saini <premchandsaini779@gmail.com>
2024-02-29 20:46:11 +0530

    :bug: Update for telescope

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,4 +6,11 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
 }
