0cd1639eb426905d3bf967eb01028aab58594ba7
Prem Chand Saini <premchandsaini779@gmail.com>
2024-02-29 20:30:53 +0530

    :white_check_mark: Complete Test Cases Deposit, Withdraw and Transfer

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,11 +6,4 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
-    }
 }
